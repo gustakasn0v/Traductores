319edababe0f699905553f86c4e7b2ab01819c8d
Wilmer Rafael Bandres Hernandez <wilmer@linux-q2r5.site>
2013-05-06 14:32:09 -0430

    mama clitoris

--- Entrega1/lexer.py
+++ Entrega1/lexer.py
@@ -25,21 +25,32 @@ palabrasReservadas = {
   'in'      : 'INST_IN',
   'do'      : 'INST_DO',
   'while'   : 'INST_while',
+  'or'      : 'OR',
+  'and'     : 'AND',
+  'not'     : 'NOT'
 }
 
 # Defino formalmente los tokens como una lista
-tokens = ['NUMBER','MINUS','EQUAL','PLUS','GREAT','GREATEQ','LESS','LESSEQ',
-          'RESERVED','VAR_IDENTIFIER'] + list(palabrasReservadas.values());
+tokens = ['NUMBER','MINUS','EQUAL','PLUS','GREAT','GREATEQ','LESS','LESSEQ','INTERSECTION',
+          'RESERVED','VAR_IDENTIFIER','COMMENT','COMMA','SEMICOLON','EQEQ','NEQEQ','IN','STRING','LPAREN','RPAREN'] + list(palabrasReservadas.values());
           
 
 # Estas son las formulas reconocedoras de tokens que solo necesiten una regexp
 t_MINUS = r'\-'
-t_EQUAL = r'\='
+t_EQUAL = r'='
 t_PLUS = r'\+'
-t_GREAT = r'\>'
-t_GREATEQ = r'\>='
-t_LESS = r'\<'
-t_LESSEQ = r'\<='
+t_GREAT = r'>'
+t_GREATEQ = r'>='
+t_LESS = r'<'
+t_LESSEQ = r'<='
+t_INTERSECTION = r'<>'
+t_EQEQ = r'=='
+t_NEQEQ = r'/='
+t_IN = r'\>\>'
+#t_STRING = r'\"([^\"\\]|(\\\")|((\\)n)|(\\))*\"'
+t_STRING = r'\"[^\"]*[^\\]\"'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 t_ignore  = ' \t \n'
 
 
@@ -51,11 +62,27 @@ def t_NUMBER(t):
   t.value = int(t.value,)
   return t
 
+def t_COMMA(t):
+  r','
+  return t
+
+def t_SEMICOLON(t):
+  r';'
+  return t
+
+#Definicion de una expresion regular para comentarios de rangeX
+def t_COMMENT(t):
+  r'//.*'
+  return t
+
+
 #Especificacion del token para palabras reservadas del lenguaje
 def t_RESERVED(t):
   r'[a-zA-Z_][a-zA-Z_0-9]*'
   t.type = palabrasReservadas.get(t.value,'VAR_IDENTIFIER')
   return t
+
+
 
 #Definicion de errores para palabras no reconocidas
 def t_error(t):
@@ -67,14 +94,17 @@ lexer = lex.lex()
 
 def main():
   a = 2
-  string = "program pedro begin declare hola end"
+  string = '''program begin declare a,b,c as range \n a = a >> b  \n write("hola")  \n ///esto es , comentario \n a/=b end \n
+"hola esto \\" es" \n es"'''
+  print string
+
   lexer.input(string)
   while 1:
     mytoken = lexer.token()
     if not mytoken:
       break
-    print "El nombre es " + mytoken.type + " y el contenido es " + str(mytoken.value)
-    
+    #print "El nombre es " + mytoken.type + " y el contenido es " + str(mytoken.value)
+    print mytoken
 
 if __name__ == "__main__":
   main()
