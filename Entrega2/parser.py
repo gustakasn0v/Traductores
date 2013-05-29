b7a4a2e71d3124752b1f01c2ee1093642d881202
LDC - Gustavo El Khoury <gustavo@ldc.usb.ve>
2013-05-29 00:38:00 -0430

    Iniciado el enfoque top-down con la primera regla Agregados documentos

---
+++ Entrega2/parser.py
@@ -0,0 +1,14 @@
+# Proyecto Traductores e Interpretadores. Entrega 2
+# Realizado por:
+# Wilmer Bandres. 1010055
+# Gustavo El Khoury. 1010226
+# -*- coding: utf-8 -*-
+
+import ply.yacc as yacc
+from lexer import tokens
+
+def p_program_structure(p):
+    'programa : INST_PROGRAM VAR_IDENTIFIER; '
+    
+    yacc.yacc()
+    
