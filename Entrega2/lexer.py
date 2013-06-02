f538eb6bb920c271f416168c530ea36fda17a8e2
Gustavo El Khoury <gustavoelkhoury@gmail.com>
2013-06-02 14:47:04 -0430

    Completada la gramática. Faltan las expresiones booleanas,aritméticas y rangos

--- Entrega2/lexer.py
+++ Entrega2/lexer.py
@@ -41,7 +41,7 @@ palabrasReservadas = {
 
 # Defino formalmente los tokens como una lista, y le concateno el diccionario
 tokens = ['NUMBER','MINUS','EQUAL','TIMES','DIVIDE','MOD','PLUS','GREAT','GREATEQ','LESS','LESSEQ','INTERSECTION',
-          'RESERVED','RANGE','VAR_IDENTIFIER','COMMENT','COMMA','SEMICOLON','EQEQ','NEQEQ','IN','STRING','LPAREN','RPAREN'] + list(palabrasReservadas.values());
+          'RESERVED','RANGE','VAR_IDENTIFIER','COMMENT','COMMA','SEMICOLON','EQEQ','NEQEQ','IN','STRING','LPAREN','RPAREN','CASE_ASSIGN'] + list(palabrasReservadas.values());
           
 
 # Estas son las formulas reconocedoras de tokens que solo necesiten una regexp
@@ -62,6 +62,7 @@ t_IN = r'\>\>'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_RANGE = r'\.\.'
+t_CASE_ASSIGN = r'\-\>'
 t_ignore  = ' \t'
 
 
