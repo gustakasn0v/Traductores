8f7ea4b2ebd7529f57984b9f45cce90b76d414e6
LDC - Gustavo El Khoury <gustavo@serena.ldc.usb.ve>
2013-06-05 00:14:17 -0430

    Agregadas las reglas

--- Entrega2/parser.py
+++ Entrega2/parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Proyecto Traductores e Interpretadores. Entrega 2
 # Realizado por:
 # Wilmer Bandres. 1010055
@@ -5,14 +6,86 @@
 # -*- coding: utf-8 -*-
 
 import ply.yacc as yacc
+import ply.lex as lex
 from lexer import tokens
 
 
 def p_program(p):
-    'program : INST_PROGRAM VAR_IDENTIFIER SEMICOLON'
-    p[0] = p[1] + p[2] + p[3]
+    'program : INST_PROGRAM Bloque_Inst'
+    p[0] = p[2]
     #print p
     
+def p_Bloque_Inst(p):
+    '''Bloque_Inst : INST_BEGIN Lista_Inst INST_END
+    | Inst'''
+    if p[1]=='begin':
+      p[0] = 'BLOQUE' + '\n'+'\t' + p[2]
+    else:
+      p[0] = p[1]
+
+def p_Lista_Inst(p):
+    '''Lista_Inst : Inst 
+    | Inst SEMICOLON Lista_Inst'''
+    if(len(p)>=3):
+      p[0] = p[1]  + p[3]
+    else:
+      p[0] = p[1]
+
+def p_Inst(p):
+  '''Inst : Inst_Declare '''
+  #| Inst_Asignacion 
+  #| Inst_Lectura 
+  #| Inst_Salida
+  #| Inst_If 
+  #| Inst_Case 
+  #| Inst_For 
+  #| Inst_While'''
+  p[0] = p[1]
+
+
+def p_Inst_Declare(p):
+  '''Inst_Declare : INST_DECLARE Lista_Declare'''
+  p[0] = p[1] + ' ' + p[2] + '\n' + '\t'
+  
+def p_Lista_Declare(p):
+  '''Lista_Declare : Lista_Variables INST_AS Tipo'''
+  p[0] = p[1] + ' ' + p[3]
+  
+def p_Lista_Variables(p):
+  '''Lista_Variables : VAR_IDENTIFIER
+  | VAR_IDENTIFIER COMMA Lista_Variables '''
+  
+  if(len(p)>=3):
+    p[0] = p[1] + ' ' + p[2] + ' ' + p[3]
+  else:
+    p[0] = p[1]
+  
+def p_Tipo(p):
+  ''' Tipo : TYPEDEF_INT 
+  | TYPEDEF_BOOL 
+  | TYPEDEF_RANGE '''
+  p[0] = 'de tipo ' + p[1]
+  
+#def p_Inst_Asignacion(p):
+  #'''Inst_Asignacion : '''
+  
+#def p_Inst_Lectura(p):
+  #'''Inst_Lectura : '''
+  
+#def p_Inst_Salida(p):
+  #'''Inst_Salida : '''
+
+#def p_Inst_If(p):
+  #'''Inst_If : '''
+
+#def p_Inst_Case(p):
+  #'''Inst_Case : '''
+
+#def p_Inst_For(p):
+  #'''Inst_For : '''
+
+#def p_Inst_While(p):
+  #'''Inst_While : '''
 
 def p_error(p):
     print "Syntax error in input!"
@@ -20,18 +93,16 @@ def p_error(p):
 # Build the parser
 parser = yacc.yacc()
 
-while True:
+s = str(open(str('entrada.txt'),'r').read())
+#while True:
     
    #try:
-   #    s = raw_input('calc > ')
+      #s = raw_input('calc > ')
    #except EOFError:
-   #    break
+      #break
    #if not s: continue
-   string = str(open('prueba1.txt','r').read())
-   print string
-   
-   result = parser.parse(string)
-   print result
+result = parser.parse(s)
+print result
 
 
     
--- Entrega2/parsetab.py
+++ Entrega2/parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.2'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'p\x02(oM\xb9\xc6EF\x92\x15\xdf:F&\xee'
+_lr_signature = '@Zi$,\xca\x17\xb5D\x05c&NK\x84\xff'
     
-_lr_action_items = {'VAR_IDENTIFIER':([2,],[3,]),'INST_PROGRAM':([0,],[2,]),'SEMICOLON':([3,],[4,]),'$end':([1,4,],[0,-1,]),}
+_lr_action_items = {'TYPEDEF_INT':([14,],[18,]),'INST_DECLARE':([2,7,15,],[5,5,5,]),'SEMICOLON':([3,10,11,18,19,20,21,],[-6,-7,15,-11,-8,-13,-12,]),'TYPEDEF_RANGE':([14,],[20,]),'TYPEDEF_BOOL':([14,],[21,]),'VAR_IDENTIFIER':([5,13,],[8,8,]),'INST_END':([3,10,11,12,18,19,20,21,22,],[-6,-7,-4,16,-11,-8,-13,-12,-5,]),'INST_AS':([8,9,17,],[-9,14,-10,]),'COMMA':([8,],[13,]),'INST_BEGIN':([2,],[7,]),'INST_PROGRAM':([0,],[2,]),'$end':([1,3,4,6,10,16,18,19,20,21,],[0,-6,-1,-3,-7,-2,-11,-8,-13,-12,]),}
 
 _lr_action = { }
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'program':([0,],[1,]),}
+_lr_goto_items = {'Inst_Declare':([2,7,15,],[3,3,3,]),'Tipo':([14,],[19,]),'Bloque_Inst':([2,],[4,]),'program':([0,],[1,]),'Inst':([2,7,15,],[6,11,11,]),'Lista_Variables':([5,13,],[9,17,]),'Lista_Declare':([5,],[10,]),'Lista_Inst':([7,15,],[12,22,]),}
 
 _lr_goto = { }
 for _k, _v in _lr_goto_items.items():
@@ -26,5 +26,17 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> INST_PROGRAM VAR_IDENTIFIER SEMICOLON','program',3,'p_program','/export/home/gustavo/git/Traductores/Entrega2/parser.py',12),
+  ('program -> INST_PROGRAM Bloque_Inst','program',2,'p_program','parser.py',14),
+  ('Bloque_Inst -> INST_BEGIN Lista_Inst INST_END','Bloque_Inst',3,'p_Bloque_Inst','parser.py',19),
+  ('Bloque_Inst -> Inst','Bloque_Inst',1,'p_Bloque_Inst','parser.py',20),
+  ('Lista_Inst -> Inst','Lista_Inst',1,'p_Lista_Inst','parser.py',27),
+  ('Lista_Inst -> Inst SEMICOLON Lista_Inst','Lista_Inst',3,'p_Lista_Inst','parser.py',28),
+  ('Inst -> Inst_Declare','Inst',1,'p_Inst','parser.py',35),
+  ('Inst_Declare -> INST_DECLARE Lista_Declare','Inst_Declare',2,'p_Inst_Declare','parser.py',47),
+  ('Lista_Declare -> Lista_Variables INST_AS Tipo','Lista_Declare',3,'p_Lista_Declare','parser.py',51),
+  ('Lista_Variables -> VAR_IDENTIFIER','Lista_Variables',1,'p_Lista_Variables','parser.py',55),
+  ('Lista_Variables -> VAR_IDENTIFIER COMMA Lista_Variables','Lista_Variables',3,'p_Lista_Variables','parser.py',56),
+  ('Tipo -> TYPEDEF_INT','Tipo',1,'p_Tipo','parser.py',64),
+  ('Tipo -> TYPEDEF_BOOL','Tipo',1,'p_Tipo','parser.py',65),
+  ('Tipo -> TYPEDEF_RANGE','Tipo',1,'p_Tipo','parser.py',66),
 ]
