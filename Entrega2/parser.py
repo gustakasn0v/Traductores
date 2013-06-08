8817a01cce26030f970d80024c64febbcf71eab4
Wilmer Rafael Bandres Hernandez <wilmer@linux-q2r5.site>
2013-06-08 12:25:58 -0430

    version sabado temprano

--- Entrega2/parser.py
+++ Entrega2/parser.py
@@ -9,6 +9,12 @@ import ply.yacc as yacc
 import ply.lex as lex
 from lexer import tokens
 
+   
+def p_program(p):
+    'program : INST_PROGRAM Bloque_Inst'
+    p[0] = p[2]
+    
+
 class bloque:
   def __init__(self,nombre,contenido):
     self.nombre = nombre
@@ -18,40 +24,6 @@ class bloque:
     print self.nombre
     self.contenido.printArbol()
     
-class bloqueDeclaracion:
-  def __init__(self,listaDeclaraciones):
-    # Esta es una lista de cada linea del declare
-    self.listaDeclaraciones = listaDeclaraciones
-    
-  def printArbol(indent):
-    print('DECLARACION:')
-    
-class unaDeclaracion:
-  def __init__(self,listaVariables,tipo):
-    self.listaVariables = listaVariables
-    self.tipo = tipo
-    
-  def printArbol(self,indent):
-    None
-
-class listaVariables:
-  def __init__(self,lista):
-    self.lista = lista
-    
-  def printArbol(self):
-    None
-    
-class listaInstrucciones:
-  def __init__(self,listaInst):
-    self.listaInst = listaInst
-    
-  def printArbol(self):
-    None
-   
-def p_program(p):
-    'program : INST_PROGRAM Bloque_Inst'
-    p[0] = p[2]
-    #print p
     
 def p_Bloque_Inst(p):
     '''Bloque_Inst : INST_BEGIN Lista_Inst INST_END
@@ -60,7 +32,18 @@ def p_Bloque_Inst(p):
       p[0] = bloque('BLOQUE',p[2])
     else:
       p[0] = bloque('UNICA INSTRUCCION',p[1])
+    
 
+
+class listaInstrucciones:
+  def __init__(self,listaInst):
+    self.listaInst = listaInst
+    
+  def printArbol(self):
+    for i in self.listaInst:
+      i.printArbol()
+      
+      
 def p_Lista_Inst(p):
     '''Lista_Inst : Inst 
     | Inst SEMICOLON Lista_Inst'''
@@ -76,25 +59,56 @@ def p_Inst(p):
   | Inst_For 
   | Inst_While
   | Inst_If 
-  | Inst_Case '''
-  #| Inst_Salida
+  | Inst_Case 
+  | Inst_Salida '''
   p[0] = p[1]
 
+  
+  
+class bloqueDeclaracion:
+  def __init__(self,listaDeclaraciones):
+    # Esta es una lista de cada linea del declare
+    self.listaDeclaraciones = listaDeclaraciones
+    
+  def printArbol(self):
+    print('DECLARACION:')
+    print self.listaDeclaraciones.printArbol()
 
+    
 def p_Inst_Declare(p):
   '''Inst_Declare : INST_DECLARE Lista_Declare'''
   p[0] = bloqueDeclaracion(p[2])
+  
+
+class unaDeclaracion:
+  def __init__(self,listaVariables,tipo):
+    self.listaVariables = listaVariables
+    self.tipo = tipo
+    
+  def printArbol(self,indent):
+    self.listaVariables.printArbol()
+
   
 def p_Lista_Declare(p):
   '''Lista_Declare : Lista_Variables INST_AS Tipo'''
   p[0] = unaDeclaracion(p[1],p[3])
   
+
+class listaVariables:
+  def __init__(self,lista):
+    self.lista = lista
+    
+  def printArbol(self):
+    for i in self.lista:
+      i.printArbol()
+    
+
 def p_Lista_Variables(p):
   '''Lista_Variables : VAR_IDENTIFIER
   | VAR_IDENTIFIER COMMA Lista_Variables '''
   
   if(len(p)>=3):
-    p[0] = [ p[3] ].insert(0,p[1])
+    p[0] = listaVariables([ p[3] ].insert(0,p[1]))
   else:
     p[0] = listaVariables([p[1]])
   
@@ -103,10 +117,19 @@ def p_Tipo(p):
   | TYPEDEF_BOOL 
   | TYPEDEF_RANGE '''
   p[0] = 'de tipo ' + p[1]
+
+class Asignacion:
+  def __init__(self,variable,expresion):
+    self.variable = variable
+    self.expresion = expresion
   
+  def printArbol(self):
+    print " a " + self.variable + " se le asigna " + self.expresion.printArbol()
+
+    
 def p_Inst_Asignacion(p):
   '''Inst_Asignacion : VAR_IDENTIFIER EQUAL Expresion'''
-  p[0] = p[1] + ' ' + p[2] + ' ' + p[3]
+  p[0] = Asignacion(p[1],p[3])
 
 
 precedence = (
@@ -119,13 +142,16 @@ precedence = (
 
 
 class Operacion:
-	def __init__(self,left,opr,right):
-		this.left = left
-		this.opr = opr
-		this.right = right
-	def printOperacion(self):
-		print "Operacion binaria:\n" + "Operador: " + this.opr + "\n" + "Operando izquierdo: " + this.left + "\nOperador Derecho: " + this.right + "\n"
-	
+  def __init__(self,left,opr="",right=""):
+    this.left = left
+    this.opr = opr
+    this.right = right
+  def printArbol(self):
+    if self.opr!="":
+      print "Operacion binaria:\n" + "Operador: " + self.opr + "\n" + "Operando izquierdo: " + self.left.printArbol() + "\nOperador Derecho: " + self.right.printArbol() + "\n"
+    else:
+      print self.left
+  
   
 def p_Expresion(p):
   '''Expresion : Operacion_binaria
@@ -135,21 +161,19 @@ def p_Expresion(p):
 
 def p_Operacion_booleana(p):
   ''' Operacion_booleana : Operacion_binaria Opr_bool Operacion_binaria
-  | Operacion_binaria Opr_bool Operacion_booleana
-  | Operacion_booleana Opr_bool Operacion_binaria
-  | Operacion_booleana Opr_bool Operacion_booleana 
+  | Operacion_booleana AND Operacion_booleana 
+  | Operacion_booleana OR Operacion_booleana 
   | 't'
   | 'f' '''
 
   if len(p)>=3:
 	  p[0] = Operacion(p[1],p[2],p[3])
   else: 
-	  p[0] = p[1]
+	  p[0] = Operacion(p[1])
 
+	  
 def p_Opr_bool(p):
-  ''' Opr_bool : OR
-  | AND
-  | EQEQ
+  ''' Opr_bool : EQEQ
   | NEQEQ
   | GREAT
   | LESS
@@ -164,7 +188,7 @@ def p_Operacion_binaria(p):
   if len(p) >= 3:
 	  p[0] = Operacion(p[1],p[2],p[3])
   else:
-	  p[0] = p[1]
+	  p[0] = Operacion(p[1])
 
 def p_Term(p):
   '''Term : Term TIMES Factor
@@ -173,7 +197,7 @@ def p_Term(p):
   if len(p) >=3:
 	  p[0] = Operacion(p[1],p[2],p[3])
   else:
-	  p[0] = p[1]
+	  p[0] = Operacion(p[1])
 
 def p_Factor(p):
   ''' Factor : NUMBER
@@ -182,7 +206,7 @@ def p_Factor(p):
   if len(p)>=3:
 	  p[0] = p[2]
   else:
-	  p[0] = p[1]
+	  p[0] = Operacion(p[1])
 
 def p_Rango(p):
   ''' Rango : Operacion_binaria RANGE Operacion_binaria
@@ -199,21 +223,57 @@ def p_RangoF(p):
   ''' RangoF : LPAREN Rango RPAREN'''
   p[0] = p[2]
 
+
+class Lectura:
+  def __init__(self,var):
+    self.variable = var
+  
+  def printArbol(self):
+    print "Read: " + '\n ' + self.var
+    
   
 def p_Inst_Lectura(p):
-  '''Inst_Lectura : INST_READ VAR_IDENTIFIER'''
-  p[0] = 'READ:' + '\n' + '\t\t' + p[2]
- 
+  '''Inst_Lectura : INST_READ VAR_IDENTIFIER '''
+  p[0] = Lectura(var)
   
-#def p_Inst_Salida(p):
-  #'''Inst_Salida : '''
+  
+class Salida:
+  def __init__(self,tipow,lista):
+    self.tipo = tipow 
+    self.lista = lista
+  
+  def printArbol(self):
+    print tipow + "\n"
+    for i in lista:
+      i.printArbol()
+    
+
+
+def p_Inst_Salida(p):
+  '''Inst_Salida : INST_WRITE Lista_Aux
+  | INST_WRITELN  Lista_Aux '''
+  
+
+def p_Lista_Aux(p):
+  '''Lista_Aux : Expresion
+  | STRING 
+  | Expresion COMMA Lista_Aux
+  | STRING COMMA Lista_Aux '''
+  
 
 class ifc:
-	def __init__(self,cond,bloque,bloque2=""):
-		this.cond = cond
-		this.bloque = bloque
-		this.bloque2 = bloque2
-
+  def __init__(self,cond,bloque,bloque2=""):
+    this.cond = cond
+    this.bloque = bloque
+    this.bloque2 = bloque2
+    
+  def printArbol(self):
+    print "Condicional if \n Condicion: "
+    print self.cond.printArbol()
+    print "Bloque: " + self.bloque.printArbol()
+    if bloque2!="":
+      print "Bloque 2: " + self.bloque2
+    
 def p_Inst_If(p):
   '''Inst_If : INST_IF Operacion_booleana INST_THEN Bloque_Inst 
   | INST_IF Operacion_booleana INST_THEN Bloque_Inst INST_ELSE Bloque_Inst'''
@@ -230,7 +290,7 @@ def p_Inst_Case(p):
 def p_Casos(p):
   ''' Casos : VAR_IDENTIFIER '-' '>' Bloque_Inst 
   | Rango '-' '>' Bloque_Inst 
-  | Casos Casos'''
+  | Casos Casos '''
 
 class forc:
 	def __init__(self,var,rango,inst):
@@ -254,6 +314,7 @@ def p_Inst_While(p):
 def p_error(p):
     print "Syntax error in input!"
 
+
 # Build the parser
 parser = yacc.yacc()
 
@@ -267,6 +328,3 @@ s = str(open(str('entrada.txt'),'r').read())
    #if not s: continue
 result = parser.parse(s)
 print result
-
-
-    
